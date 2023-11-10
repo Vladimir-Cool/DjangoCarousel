7159ade14856a001f58502cc44bc65a193354801
Vladimir <58716739+Vladimir-Cool@users.noreply.github.com>
2023-11-10 10:41:27 +0800

    Для теста вывел в шаблоне index.html Все записи модели CarouselModel.

--- DjangoCarousel/carousel/views.py
+++ DjangoCarousel/carousel/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import CarouselModel
+
 def get_home_page_with_carousel(request: HttpRequest) -> HttpResponse:
     """ Функция венрен домашнюю страницу с 'каруселью' картинок"""
     # Забрать контекст из модели
+
+    query_set = CarouselModel.objects.all()
+
     context = {"data": "Тут будет Data"}
-    return render(request, "carousel/carousel.html", context=context)
+    return render(request, "carousel/carousel.html", context={"data": query_set})
